05687d75815abebddf07c26910a69dcf3da4021f
Oltion <cucioltion6@gmail.com>
2025-09-01 20:14:47 +0200

    prod:2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "OV AUTOMOTIVE GROUP",
@@ -25,7 +26,7 @@ html {
 }
         `}</style>
       </head>
-      <body>{children}</body>
+      <body>{children}<Analytics /></body>
     </html>
   );
 }
